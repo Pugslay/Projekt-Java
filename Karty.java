118babb63e441f785170fd501b2ac3e75f48e982
Pugslay <53711206+Pugslay@users.noreply.github.com>
2023-05-17 12:30:09 +0200

    Update Karty.java

--- Karty.java
+++ Karty.java
@@ -7,26 +7,44 @@ import java.util.Scanner;
 public class Karty {
 
     static Random rand = new Random();
-    static int[] id = new int[32];
-    static int[] atak = new int[32];
-    static int[] hp = new int[32];
-    static int[] pancerz = new int[32];
-    static int[] koszt = new int[32];
-    static String[] efekt = new String[32];
-    static String[] efekt_2 = new String[32];
-    static String[] nazwa = new String[32];
+    static int[] id = new int[31];
+    static int[] atak = new int[31];
+    static int[] hp = new int[31];
+    static int[] pancerz = new int[31];
+    static int[] koszt = new int[31];
+    static String[] efekt = new String[31];
+    static String[] efekt_2 = new String[31];
+    static String[] nazwa = new String[31];
 
     static int y = 3;
     static int x = 5;
     static int[][] pozycja = new int[y][x];
 
-    static int[] reka = new int[5];
+    static int[][] reka = new int[5][2];
     static int mana;
     static int mana_gr;
     static int licz = 0;
-    static boolean schlop;
+    static int HP_GRACZ_1=2;
+    static int HP_GRACZ_2=2;
 
-
+    public static boolean sprawdz_hp(){
+    if(HP_GRACZ_1<=0){
+        if(HP_GRACZ_2<=0) {
+            System.out.println("REMIS");
+            return true;
+        }
+        else {
+            System.out.println("Wygrywa gracz nr 2");
+            return true;
+        }
+    }
+    else{
+        if(HP_GRACZ_2<=0){
+            System.out.println("Wygrywa gracz nr 1");
+        }
+    }
+    return false;
+    }
     public static void main(String[] args) throws SQLException {
         pobierz();
         pusty();
@@ -38,36 +56,66 @@ public class Karty {
 
         int wyj = 0;
         mana = 1;
+        int gracz = 0;
         while (wyj != 10) {
+            if(sprawdz_hp()){
+            break;
+            }
+
             mana_gr = mana;
 
+            System.out.println("Tura: "+(wyj+1));
+            System.out.println("Gracz: "+(gracz+1));
             System.out.print("""
                     Co chcesz zrobić:\s
                     1. Sprawdz stan stołu
                     2. Sprawdz mane
                     3. Sprawdz rękę
                     4. Rzuc karte
-                    5. Skończ turę
+                    5. Sprawdz HP
+                    6. Skończ turę
                     """);
             Scanner scan = new Scanner(System.in);
             int a = scan.nextInt();
 
             switch (a) {
                 case 1 -> sprawdz();
-                case 2 -> System.out.println("Twoja mana wynosi: " + mana_gr);
+                case 2 -> System.out.println("Twoja mana wynosi: " + mana_gr/mana);
                 case 3 -> {
-                    sprawdz_Reka();
+                    sprawdz_Reka(gracz);
                     System.out.println();
                 }
                 case 4 -> {
-                    wybor();
-                    Chlopy();
+                    wybor(gracz);
+                    System.out.println(licz);
+                    //Chlopy();
+                    for (int j = 0; j < 4; j++) {
+                        if(efekt[pozycja[0][j]].equals("CHLP")){
+                            licz++;
+                        }
+                    }
+                    if (licz >= 2) {
+                        for (int j = 0; j < 4; j++) {
+                            if (efekt[pozycja[0][j]].equals("CHLP")) {
+                                hp[pozycja[0][j]] = hp[pozycja[0][j]] * 2;
+                                atak[pozycja[0][j]] = atak[pozycja[0][j]] * 2;
+                                System.out.println(hp[pozycja[0][j]]+" "+hp[pozycja[0][j]]);
+                            }
+                        }
+                    }
                 }
                 case 5 -> {
+                    System.out.println("Zdrowie gracza 1 wynosi: "+HP_GRACZ_1);
+                    System.out.println("Zdrowie gracza 2 wynosi: "+HP_GRACZ_2);
+                }
+                case 6 -> {
                     wyj++;
                     mana++;
                     koniec_tury();
-                    sprawdz_l_kart();
+                    sprawdz_l_kart(gracz);
+                    gracz++;
+                    if(gracz>=2)
+                        gracz=0;
                 }
                 default -> System.out.println("NIE MA TAKIEJ OPCJI");
             }
@@ -75,7 +123,7 @@ public class Karty {
         }
     }
 
-    public static void pobierz() throws SQLException {
+    public static void pobierz() throws SQLException { // tutaj
         int n = 1;
 
         String url = "jdbc:oracle:thin:@localhost:1521:XE";
@@ -101,8 +149,8 @@ public class Karty {
             System.out.println("No results found."); // wyświetla komunikat, jeśli zapytanie nie zwróciło wyników
         }
 
-        for (int a = 0; a < 30; a++) {
-            System.out.println("ID: " + id[a] + ", ATA: " + atak[a] + ", HP: " + hp[n] + ", PANCERZ: " + pancerz[a] + ", KOSZT: " + koszt[a] + ", NAZWA: " + nazwa[a] + ", EFEKTY: " + efekt[a] + " " + efekt_2[a]);
+        for (int a = 0; a < 31; a++) {
+            System.out.println("ID: " + id[a] + ", ATA: " + atak[a] + ", HP: " + hp[a] + ", PANCERZ: " + pancerz[a] + ", KOSZT: " + koszt[a] + ", NAZWA: " + nazwa[a] + ", EFEKTY: " + efekt[a] + " " + efekt_2[a]);
         }
 
         resultSet.close();
@@ -120,7 +168,7 @@ public class Karty {
         for (int i = 0; i < 2; i++) {
             System.out.println();
             for (int j = 0; j < 4; j++) {
-                System.out.print(nazwa[pozycja[i][j]] + " | ");
+                System.out.print(nazwa[pozycja[i][j]] + " " + atak[pozycja[i][j]] + " | " );
             }
         }
 
@@ -128,33 +176,65 @@ public class Karty {
 
     //losuje reke
     public static void losuj_reka() {
+        for(int j = 0;j<=1;j++){
         for (int i = 0; i <= 4; i++) {
             int a = rand.nextInt(1, 31);
-            reka[i] = id[a];
-        }
+            reka[i][j] = id[a];
+        }}
     }
 
     //sprawdza reke gracza
-    public static void sprawdz_Reka() {
+    public static void sprawdz_Reka(int j) {
         System.out.println("Twoja ręka:");
         for (int i = 0; i <= 4; i++) {
-            System.out.println((i + 1) + "." + nazwa[reka[i]] + " HP:" + hp[reka[i]] + " ATAK:" + atak[reka[i]] + " OBRONA:" + pancerz[reka[i]] + " KOSZT:" + koszt[reka[i]] + " EFEKTY:" + efekt[reka[i]] + " | " + efekt_2[reka[i]]);
+            System.out.println((i + 1) + "." + nazwa[reka[i][j]] + " HP:" + hp[reka[i][j]] + " ATAK:" + atak[reka[i][j]] + " OBRONA:" + pancerz[reka[i][j]] + " KOSZT:" + koszt[reka[i][j]] + " EFEKTY:" + efekt[reka[i][j]] + " | " + efekt_2[reka[i][j]]);
 
         }
     }
 
     //wybor karty z reki
-    public static void wybor() {
-        sprawdz_Reka();
+    public static void wybor(int j) {
+        sprawdz_Reka(j);
         System.out.print("Wybierz karte: \n");
         Scanner scan = new Scanner(System.in);
         int a = scan.nextInt();
         switch (a) {
-            case 1 -> miejsce(0);
-            case 2 -> miejsce(1);
-            case 3 -> miejsce(2);
-            case 4 -> miejsce(3);
-            case 5 -> miejsce(4);
+            case 1 -> {
+                if(koszt[reka[0][j]]<=mana_gr){
+                    mana_gr=mana_gr-koszt[reka[0][j]];
+                    miejsce(0,j);}
+                else
+                    System.out.println("Nie masz tyle many!");
+            }
+            case 2 -> {
+                if(koszt[reka[1][j]]<=mana_gr){
+                    mana_gr=mana_gr-koszt[reka[1][j]];
+                    miejsce(1,j);}
+                else
+                    System.out.println("Nie masz tyle many!");
+            }
+            case 3 -> {
+                if(koszt[reka[2][j]]<=mana_gr){
+                    mana_gr=mana_gr-koszt[reka[2][j]];
+                    miejsce(2,j);}
+                else
+                    System.out.println("Nie masz tyle many!");
+            }
+            case 4 -> {
+                if(koszt[reka[3][j]]<=mana_gr){
+                    mana_gr=mana_gr-koszt[reka[1][j]];
+                    miejsce(3,j);}
+                else
+                    System.out.println("Nie masz tyle many!");
+            }
+            case 5 -> {
+                if(koszt[reka[4][j]]<=mana_gr){
+                    mana_gr=mana_gr-koszt[reka[1][j]];
+                    miejsce(4,j);
+                }
+                else
+                    System.out.println("Nie masz tyle many!");
+            }
             default -> System.out.println("NIE MA TAKIEJ OPCJI");
         }
 
@@ -162,7 +242,7 @@ public class Karty {
     }
 
     //pozwala na rzucenie karty na dane WOLNE miejsce
-    public static void miejsce(int Kid) {
+    public static void miejsce(int Kid, int j) {
         sprawdz();
         boolean wyj = false;
         int a;
@@ -173,32 +253,32 @@ public class Karty {
             switch (a) {
                 case 1 -> {
                     if (sprawdz_miejsce(pozycja[0][0])) {
-                        pozycja[0][0] = reka[Kid];
-                        Sprawdz_chlopa(pozycja[0][0]);
+                        pozycja[0][0] = reka[Kid][j];
+                        reka[Kid][j]=0;
                         wyj = true;
                     } else
                         System.out.println("Tutaj jest juz karta");
                 }
                 case 2 -> {
                     if (sprawdz_miejsce(pozycja[0][1])) {
-                        pozycja[0][1] = reka[Kid];
-                        Sprawdz_chlopa(pozycja[0][1]);
+                        pozycja[0][1] = reka[Kid][j];
+                        reka[Kid][j]=0;
                         wyj = true;
                     } else
                         System.out.println("Tutaj jest juz karta");
                 }
                 case 3 -> {
                     if (sprawdz_miejsce(pozycja[0][2])) {
-                        pozycja[0][2] = reka[Kid];
-                        Sprawdz_chlopa(pozycja[0][2]);
+                        pozycja[0][2] = reka[Kid][j];
+                        reka[Kid][j]=0;
                         wyj = true;
                     } else
                         System.out.println("Tutaj jest juz karta");
                 }
                 case 4 -> {
                     if (sprawdz_miejsce(pozycja[0][3])) {
-                        pozycja[0][3] = reka[Kid];
-                        Sprawdz_chlopa(pozycja[0][3]);
+                        pozycja[0][3] = reka[Kid][j];
+                        reka[Kid][j]=0;
                         wyj = true;
                     } else
                         System.out.println("Tutaj jest juz karta");
@@ -210,9 +290,6 @@ public class Karty {
         sprawdz();
     }
 
-    public static void Sprawdz_chlopa(int a) {
-        schlop = efekt[a].equals("CHLP");
-    }
 
     //sprawdza czy dany miejsce jest zajete
     public static boolean sprawdz_miejsce(int poz) {
@@ -224,11 +301,11 @@ public class Karty {
     }
 
     //sprawdza i dodaje karte gdy brakuje karty
-    public static void sprawdz_l_kart() {
+    public static void sprawdz_l_kart(int j) {
         for (int i = 0; i <= 4; i++) {
-            if (nazwa[reka[i]] == null) {
+            if (nazwa[reka[i][j]] == "NIC") {
                 int a = rand.nextInt(1, 31);
-                reka[i] = id[a];
+                reka[i][j] = id[a];
                 break;
             }
 
@@ -237,54 +314,122 @@ public class Karty {
 
     public static void koniec_tury() {
         for (int j = 0; j <= 4; j++) {
-            //Przebicie
-            System.out.println(efekt[pozycja[0][j]]+"|"+efekt_2[pozycja[0][j]]);
-            if (efekt[pozycja[0][j]].equals("p1") || efekt_2[pozycja[0][j]].equals("p1"))
-                hp[pozycja[0][j]] = hp[pozycja[0][j]] - (atak[pozycja[1][j]] - (pancerz[pozycja[0][j]] - 1));
-            else {
-                if (efekt[pozycja[0][j]].equals("p2") || efekt_2[pozycja[0][j]].equals("p2")) {
-                    hp[pozycja[0][j]] = hp[pozycja[0][j]] - (atak[pozycja[1][j]] - (pancerz[pozycja[0][j]] - 2));
+
+            if(nazwa[pozycja[0][j]]=="NIC"&&nazwa[pozycja[1][j]]!="NIC")
+                HP_GRACZ_1=HP_GRACZ_1-atak[pozycja[1][j]];
+
+            if(nazwa[pozycja[1][j]]=="NIC"&&nazwa[pozycja[0][j]]!="NIC")
+                HP_GRACZ_2=HP_GRACZ_2-atak[pozycja[0][j]];
+
+            //Dystansowe
+            if(efekt[pozycja[0][j]].equals("DYS")){//T
+                if(efekt[pozycja[1][j]].equals("DYS"))//TT
+                    przeb(j);
+                else{//TN
+                //Gracz 1 ma atak dystansowy
+                if(efekt[pozycja[0][j]].equals("P1") || efekt_2[pozycja[0][j]].equals("P1")){
+                    if(atak[pozycja[0][j]] - (pancerz[pozycja[1][j]]-1)>0)
+                        hp[pozycja[1][j]] = hp[pozycja[1][j]] - (atak[pozycja[0][j]] - (pancerz[pozycja[1][j]]-1));}
+                else{
+                    if(efekt[pozycja[0][j]].equals("P2") || efekt_2[pozycja[0][j]].equals("P2")){
+                        if(atak[pozycja[0][j]] - (pancerz[pozycja[1][j]]-2)>0)
+                            hp[pozycja[1][j]] = hp[pozycja[1][j]] - (atak[pozycja[0][j]] - (pancerz[pozycja[1][j]]-2));}
+                }
+
+                    if(atak[pozycja[0][j]] - pancerz[pozycja[1][j]]>0)
+                        hp[pozycja[1][j]] = hp[pozycja[1][j]] - (atak[pozycja[0][j]] - pancerz[pozycja[1][j]]);
+
+                if (hp[pozycja[1][j]] <= 0 && nazwa[pozycja[0][j]] != "NIC") {
+                    System.out.println(nazwa[pozycja[1][j]] + " została zniszczona\n");
+                    pozycja[1][j] = 0;
+                    continue;}
+                else{ //atak karty gracza 2 po dystansowym
+                    if(efekt[pozycja[1][j]].equals("P1") || efekt_2[pozycja[1][j]].equals("P1")){
+                        if(atak[pozycja[1][j]] - (pancerz[pozycja[0][j]]-1)>0)
+                            hp[pozycja[0][j]] = hp[pozycja[0][j]] - (atak[pozycja[1][j]] - (pancerz[pozycja[0][j]]-1));}
+                    else{
+                        if(efekt[pozycja[1][j]].equals("P2") || efekt_2[pozycja[1][j]].equals("P2")){
+                            if(atak[pozycja[1][j]] - (pancerz[pozycja[0][j]]-2)>0)
+                                hp[pozycja[0][j]] = hp[pozycja[0][j]] - (atak[pozycja[1][j]] - (pancerz[pozycja[0][j]]-2));}
+                    }
+
+                    if(atak[pozycja[1][j]] - pancerz[pozycja[0][j]]>0)
+                        hp[pozycja[0][j]] = hp[pozycja[0][j]] - (atak[pozycja[1][j]] - pancerz[pozycja[0][j]]);
+
+                    if (hp[pozycja[0][j]] <= 0 && nazwa[pozycja[1][j]] != "NIC") {
+                        System.out.println(nazwa[pozycja[0][j]] + " została zniszczona\n");
+                        pozycja[0][j] = 0;
+                        continue;}
+                }
                 }
             }
+            else{
+                if(!efekt[pozycja[1][j]].equals("DYS"))
+                    przeb(j);
+                else{ //atak karty gracza 2 po dystansowym
+                    if(efekt[pozycja[0][j]].equals("P1") || efekt_2[pozycja[0][j]].equals("P1")){
+                        if(atak[pozycja[0][j]] - (pancerz[pozycja[1][j]]-1)>0)
+                            hp[pozycja[1][j]] = hp[pozycja[1][j]] - (atak[pozycja[0][j]] - (pancerz[pozycja[1][j]]-1));}
+                    else{
+                        if(efekt[pozycja[0][j]].equals("P2") || efekt_2[pozycja[0][j]].equals("P2")){
+                            if(atak[pozycja[0][j]] - (pancerz[pozycja[1][j]]-2)>0)
+                                hp[pozycja[1][j]] = hp[pozycja[1][j]] - (atak[pozycja[0][j]] - (pancerz[pozycja[1][j]]-2));}
+                    }
 
-            //sprawdzenie czy karta zostala zniszczona
-            if (hp[pozycja[0][j]] <= 0 && nazwa[pozycja[0][j]] != null) {
-                System.out.println(nazwa[pozycja[0][j]] + " została zniszczona\n");
-                pozycja[0][j] = 0;
+                    if(atak[pozycja[0][j]] - pancerz[pozycja[1][j]]>0)
+                        hp[pozycja[1][j]] = hp[pozycja[1][j]] - (atak[pozycja[0][j]] - pancerz[pozycja[1][j]]);
+
+                    if (hp[pozycja[1][j]] <= 0 && nazwa[pozycja[0][j]] != "NIC") {
+                        System.out.println(nazwa[pozycja[1][j]] + " została zniszczona\n");
+                        pozycja[1][j] = 0;
+                        continue;}
+
+                }
             }
+        }
+    }
 
-            if (hp[pozycja[1][j]] <= 0 && nazwa[pozycja[1][j]] != null) {
-                System.out.println(nazwa[pozycja[1][j]] + " została zniszczona\n");
-                pozycja[1][j] = 0;
-                continue;
+    private static void przeb(int j) {
+        if (efekt[pozycja[0][j]].equals("p1") || efekt_2[pozycja[0][j]].equals("p1"))
+            if((hp[pozycja[1][j]] - (atak[pozycja[0][j]] - (pancerz[pozycja[1][j]]-1)))>0)
+                hp[pozycja[1][j]] = hp[pozycja[1][j]] - (atak[pozycja[0][j]] + ((pancerz[pozycja[1][j]] - 1)));
+        else {
+            if (efekt[pozycja[0][j]].equals("p2") || efekt_2[pozycja[0][j]].equals("p2")) {
+                if((hp[pozycja[1][j]] - (atak[pozycja[0][j]] - (pancerz[pozycja[1][j]]-2)))>0)
+                    hp[pozycja[1][j]] = hp[pozycja[1][j]] - (atak[pozycja[0][j]] - ((pancerz[pozycja[1][j]] - 2)));
+
             }
+            else
+            if((hp[pozycja[1][j]] - (atak[pozycja[0][j]] - pancerz[pozycja[1][j]]))>0)
+                    hp[pozycja[0][j]] = hp[pozycja[1][j]] - (atak[pozycja[0][j]] - pancerz[pozycja[1][j]]);
+        }
 
 
-            //bez przebicia
-            hp[pozycja[0][j]] = hp[pozycja[0][j]] - (atak[pozycja[1][j]] - pancerz[pozycja[0][j]]);
-            if (hp[pozycja[0][j]] <= 0 && nazwa[pozycja[0][j]] != null) {
+        if (efekt[pozycja[1][j]].equals("p1") || efekt_2[pozycja[1][j]].equals("p1")){
+            if((hp[pozycja[0][j]] - (atak[pozycja[1][j]] - (pancerz[pozycja[0][j]]-1)))>0)
+                hp[pozycja[0][j]] = hp[pozycja[0][j]] - (atak[pozycja[1][j]] + ((pancerz[pozycja[0][j]] - 1)));}
+        else {
+            if (efekt[pozycja[1][j]].equals("p2") || efekt_2[pozycja[1][j]].equals("p2")) {
+                if((hp[pozycja[0][j]] - (atak[pozycja[1][j]] - (pancerz[pozycja[0][j]]-2)))>0)
+                    hp[pozycja[0][j]] = hp[pozycja[0][j]] - (atak[pozycja[1][j]] + ((pancerz[pozycja[0][j]] - 2)));
+
+            }
+            else{
+            if((hp[pozycja[0][j]] - (atak[pozycja[1][j]] - pancerz[pozycja[0][j]]))>0)
+                hp[pozycja[0][j]] = hp[pozycja[0][j]] - (atak[pozycja[1][j]] - pancerz[pozycja[0][j]]);}
+        }
+
+            if (hp[pozycja[0][j]] <= 0 && nazwa[pozycja[0][j]] != "NIC") {
                 System.out.println(nazwa[pozycja[0][j]] + " została zniszczona\n");
                 pozycja[0][j] = 0;
             }
 
             hp[pozycja[1][j]] = hp[pozycja[1][j]] - atak[pozycja[0][j]];
-            if (hp[pozycja[1][j]] <= 0 && nazwa[pozycja[1][j]] != null) {
+            if (hp[pozycja[1][j]] <= 0 && nazwa[pozycja[1][j]] != "NIC") {
                 System.out.println(nazwa[pozycja[1][j]] + " została zniszczona\n");
                 pozycja[1][j] = 0;
             }
-        }
 
     }
 
-    public static void Chlopy() {
-        licz++;
-        if (licz >= 2) {
-            for (int j = 0; j < 4; j++) {
-                if (efekt[pozycja[0][j]] == "CHLP") {
-                    hp[pozycja[0][j]] = hp[pozycja[0][j]] * 2;
-                    atak[pozycja[0][j]] = atak[pozycja[0][j]] * 2;
-                }
-            }
-        }
-    }
 }
